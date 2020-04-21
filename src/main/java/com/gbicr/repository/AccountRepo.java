0f68a731e6d524b52733772e1b57045b3ec8fb75
Josua Pangihutan Sitanggang <josuastg93@gmail.com>
2020-04-21 18:22:57 +0700

    deleted

--- src/main/java/com/gbicr/repository/AccountRepo.java
+++
@@ -1,7 +0,0 @@
-package com.gbicr.repository;
-
-import com.gbicr.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepo extends JpaRepository<Account, String> {
-}
